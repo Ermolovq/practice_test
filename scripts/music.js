1bd9e1112abb052d2e46fe21c16212eebfac3651
Ermolovq <145741855+Ermolovq@users.noreply.github.com>
2024-04-11 13:28:37 +0300

    commit

---
+++ scripts/music.js
@@ -0,0 +1,16 @@
+const music = document.getElementById("song");
+const music_icon = document.getElementById("music-icon");
+music.volume = 0.2;
+music_icon.onclick = function () {
+    if (music.paused) {
+        music.play();
+        music_icon.src = "images/pause.png";
+    } else {
+        music.pause();
+        music_icon.src = "images/play.png";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    music.play();
+});
